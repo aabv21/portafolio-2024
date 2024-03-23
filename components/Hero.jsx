8ac249027e38eaa4b61f6f9ef95542b1a7076dfb
Andrés Buelvas <afroelectrico@gmail.com>
2024-03-23 04:58:30 -0400

    add footer component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,13 +16,12 @@ import Socials from "./Socials";
 
 const Hero = () => {
   return (
-    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-no-repeat bg-bottom bg-cover dark:bg-none">
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
           {/* text */}
           <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
             <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
-              {" "}
               Web Developer
             </div>
             <h1 className="h1 mb-4">Hello, my name is Andrés Buelvas</h1>
@@ -34,11 +33,11 @@ const Hero = () => {
             <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:xm-0 mb-12">
               <Link href="/contact">
                 <Button className="gap-x-2">
-                  Contact me <Send size={18} />{" "}
+                  Contact me <Send size={18} />
                 </Button>
               </Link>
               <Button variant="secondary" className="gap-x-2">
-                Download CV <Download size={18} />{" "}
+                Download CV <Download size={18} />
               </Button>
             </div>
             {/* socials */}
