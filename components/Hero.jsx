71261fe14f76ad9ac381f0eefc210360fc65d840
Andrés Buelvas <afroelectrico@gmail.com>
2024-03-25 06:02:44 -0400

    add base url to import statics

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,6 +14,8 @@ import DevImg from "./DevImg";
 import Badge from "./Badge";
 import Socials from "./Socials";
 
+const basePath = "/portafolio-2024";
+
 const Hero = () => {
   return (
     <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-no-repeat bg-bottom bg-cover dark:bg-none">
@@ -74,7 +76,7 @@ const Hero = () => {
             <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
             <DevImg
               containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
-              imgSrc="./hero/developer.png"
+              imgSrc={`${basePath}/hero/developer.png`}
             />
           </div>
         </div>
