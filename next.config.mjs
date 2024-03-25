38285c82a681a03795ae94510ea252cb7f7e76fa
Andrés Buelvas <afroelectrico@gmail.com>
2024-03-24 23:47:10 -0400

    fix some paths

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export", // enables static exports
   basePath: "/portafolio-2024", // for github actions deploy
-  // reactStrictMode: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
