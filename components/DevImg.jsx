699ac7b31c342e0638eec191e95caed02ce6eec7
Andrés Buelvas <afroelectrico@gmail.com>
2024-03-25 06:38:16 -0400

    add base url to import statics

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const DevImg = ({ containerStyles, imgSrc }) => {
   return (
     <div className={`${containerStyles}`}>
-      <Image src={imgSrc} fill priority alt=""></Image>
+      <Image src={imgSrc} unoptimized fill priority alt=""></Image>
     </div>
   );
 };
