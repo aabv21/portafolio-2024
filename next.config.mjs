8e3d8234c889b70e57d1be46839f72b57be1593c
Andrés Buelvas <afroelectrico@gmail.com>
2024-03-24 23:19:47 -0400

    add .yml files for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/portafolio-2024", // for github actions deploy
+  output: "export", // enables static exports
+  reactStrictMode: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
