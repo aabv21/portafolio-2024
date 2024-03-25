e0f70cc26c5df08c85d3460833ebe0f8c970231e
Andrés Buelvas <afroelectrico@gmail.com>
2024-03-25 05:19:59 -0400

    add base url path to image attrb next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = "/portafolio-2024";
+
 const nextConfig = {
   output: "export", // enables static exports
-  basePath: "/portafolio-2024", // for github actions deploy
+  basePath, // for github actions deploy
   reactStrictMode: true,
+  images: {
+    path: `${basePath}/_next/image`,
+  },
 };
 
 // const nextConfig = {};
