2528692d3cae94d76ae82a6acc1139f7db592b8d
Andrés Buelvas <afroelectrico@gmail.com>
2024-03-25 02:50:42 -0400

    enable static exports

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export", // enables static exports
-//   basePath: "/portafolio-2024", // for github actions deploy
-// reactStrictMode: true,
-// };
+const nextConfig = {
+  output: "export", // enables static exports
+  basePath: "/portafolio-2024", // for github actions deploy
+  reactStrictMode: true,
+};
 
-const nextConfig = {};
+// const nextConfig = {};
 
 export default nextConfig;
